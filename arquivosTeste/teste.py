c85f9e92a46cf96755c572dadad5245d226cfb74
FlavioPasquetti <113318241+FlavioPasquetti@users.noreply.github.com>
2022-11-26 22:33:46 -0300

    v5.14.6

--- arquivosTeste/teste.py
+++
@@ -1,8 +0,0 @@
-from tkinter import filedialog
-
-def openFile (typeFile, exts):
-
-    return filedialog.askopenfiles(filetypes=[(typeFile, exts)])   
-
-#Teste
-files= openFile("Imagens (.png, .jpg)", ("*.png", "*.jpg"))
